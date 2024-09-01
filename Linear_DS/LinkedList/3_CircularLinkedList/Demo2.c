76a40d30f16757b2ca8c4df62854870f8e342dba
Pushkar Modi <116958355+Pushkar111@users.noreply.github.com>
2024-09-01 16:29:10 +0530

    CircularLinkedList

---
+++ Linear_DS/LinkedList/3_CircularLinkedList/Demo2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+struct node *last = NULL;
+
+struct node *AddNode(int no)
+{
+    // Allocate memory in the heap
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+
+    if (head == NULL)
+    {
+        head = (struct node *)malloc(sizeof(struct node));
+        head->data = no;
+        head->next = head;
+        last = head;
+    }
+    else
+    {
+        temp = (struct node *)malloc(sizeof(struct node));
+        temp->data = no;
+        temp->next = head;
+        last->next = temp;
+        last = temp;
+    }
+
+    return head;
+}
+
+int main()
+{
+    struct node *head = NULL;
+
+    head = AddNode(10);
+    head = AddNode(20);
+    head = AddNode(30);
+    head = AddNode(40);
+    head = AddNode(50);
+
+    printf("%d -> ", head->data);
+    printf("%d -> ", head->next->data);
+    printf("%d -> ", head->next->next->data);
+    printf("%d -> ", head->next->next->next->data);
+    printf("%d -> NULL", last->data);
+
+    return 0;
+}
